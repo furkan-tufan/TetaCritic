395e5c7041da3052f67012916f204e6cca6e6f8d
furkan-tufan <furkanismettufan@gmail.com>
2021-12-19 23:58:23 +0300

    Besinci Commit
    Admin Paneli Eklendi

---
+++ TetaCritic/TetaCritic/TetaCritic/Models/Film.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TetaCritic.Models
+{
+    public class Film
+    {
+        [Key]
+        public int FilmId { get; set; }
+        [Display(Name = "Yönetmen")]
+        public string Yonetmen { get; set; }
+        [Display(Name = "Özet")]
+        public string Ozet { get; set; }
+       // [DataType(DataType.Date)]
+        [Display(Name = "Vizyon Tarihi")]
+        public int VizyonTarihi { get; set; }
+        [Display(Name = "Oyuncu Kadrosu")]
+        public List<string> Oyuncular = new List<string>();
+        [Display(Name = "Kategori")]
+        public Kategori Ktg { get; set; }
+        public int KategoriId{ get; set; }
+
+    }
+}
---
+++ TetaCritic/TetaCritic/TetaCritic/Models/Kategori.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TetaCritic.Models
+{
+    public class Kategori
+    {
+        [Key]
+        public int KategoriId { get; set; }
+        [Display(Name = "Kategori Adı")]
+        public string KategoriAdi { get; set; }
+        public List<Film> FilmListesi = new List<Film>();
+    }
+}
