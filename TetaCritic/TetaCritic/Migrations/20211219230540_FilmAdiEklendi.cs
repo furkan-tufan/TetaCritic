89ff8d34fa0bc7c2fc1b5567d39df30dcd02fe90
furkan-tufan <furkanismettufan@gmail.com>
2021-12-20 06:02:38 +0300

    Altinci Commit
    Admin Paneli tasarımı güncellendi. Birçok hata giderildi.

---
+++ TetaCritic/TetaCritic/Migrations/20211219230540_FilmAdiEklendi.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TetaCritic.Migrations
+{
+    public partial class FilmAdiEklendi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FilmAdi",
+                table: "Filmler",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FilmAdi",
+                table: "Filmler",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
---
+++ TetaCritic/TetaCritic/Models/Film.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TetaCritic.Models
+{
+    public class Film
+    {
+        [Key]
+        public int FilmId { get; set; }
+        [Display(Name = "Film Adı")]
+        public string FilmAdi { get; set; }
+        [Display(Name = "Yönetmen")]
+        public string Yonetmen { get; set; }
+        [Display(Name = "Özet")]
+        public string Ozet { get; set; }
+        [Display(Name = "Çıkış Yılı")]
+        public int VizyonTarihi { get; set; }
+        [Display(Name = "Oyuncu Kadrosu")]
+        public List<string> Oyuncular = new List<string>();
+        [Display(Name = "Kategori")]
+        public Kategori Ktg { get; set; }
+        public int KategoriId{ get; set; }
+
+    }
+}
